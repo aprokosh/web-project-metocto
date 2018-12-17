9091a9e4c380b212aaa85d572feeb560aa4ab785
Anna <anutaprok@mail.ru>
2018-12-17 06:14:06 +0300

    /

--- index.js
+++ index.js
@@ -284,11 +284,9 @@ app.post('/unfav', (req, res) => {
 
 app.get('/getfav', (req, res) => {
     let meros = [];
-    console.log('meow at getting fav')
     mongoClient.connect(url, function (err, client) {
         client.db("metodbase").collection("users").findOne({login: req.session.username}, function (err, result) {
-            console.log(result.fav.length)
-            for (var i = 0; i < result.fav.length; ++i) {
+            for (var i = 1; i <= result.fav.length; ++i) {
                 client.db("metodbase").collection("mero").findOne({_id: ObjectId(result.fav[i])}, function (err, res) {
                     meros.push({
                         id: res._id,
